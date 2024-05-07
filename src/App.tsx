005a1029d2de38a3eb9fe7818ffcca0a79642135
Grigoriy1209 <grigoriyv1209@gmail.com>
2024-05-07 18:28:53 +0300

    hooks-useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import {useState} from "react";
+
+
+
+interface IState {
+    value: number
+}
+
 const App = () => {
+    const [counter, setCounter] = useState<IState>({value: 0})
+
+    const increment = () => {
+        setCounter(prevState => ({value: prevState.value + 1}))
+    }
+    const decrement = () => {
+        setCounter(prevState => ({value: prevState.value - 1}))
+
+    }
+    const reset = () => {
+        setCounter({value: 0})
+
+    }
     return (
         <div>
-            App
+
+            <h2>{counter.value}</h2>
+            <button onClick={increment}>increment</button>
+            <button onClick={decrement}>decrement</button>
+            <button onClick={reset}>reset</button>
+
         </div>
     );
 };
