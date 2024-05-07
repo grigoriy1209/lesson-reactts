6a3680da444dc5336f5f56257c822e97d65b00d0
Grigoriy1209 <grigoriyv1209@gmail.com>
2024-05-07 17:42:18 +0300

    const2

--- src/services/dummi.api.services.ts
+++ src/services/dummi.api.services.ts
@@ -1,7 +1,8 @@
 import axios, {AxiosResponse} from "axios";
 
 import {CartsResponseModel} from "../models/CartsResponseModel";
-import {ProductResponse} from "../models/ProductResponse";
+
+import {FullProductModel} from "../models/FullProductModel";
 
 const axiosInstance = axios.create({
     baseURL: "https://dummyjson.com",
@@ -10,7 +11,7 @@ const axiosInstance = axios.create({
 const getAllCarts = (): Promise<AxiosResponse<CartsResponseModel>> => {
     return axiosInstance.get('/carts')
 }
-const getProductById = (id: number):Promise<AxiosResponse<ProductResponse>> => {
+const getProductById = (id: number):Promise<AxiosResponse<FullProductModel>> => {
   return  axiosInstance.get('/products/' + id)
 }
 
